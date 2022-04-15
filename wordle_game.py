602c12b335bae0940e764ccc7de400f21d16218b
Maggie Huang <mhuang09@terpmail.umd.edu>
2022-04-15 14:10:13 -0400

    added methods and docstrings

--- wordle_game.py
+++ wordle_game.py
@@ -1,3 +1,33 @@
 "This is a wordle game"
 
 class Wordle:
+    """A program that takes in a user inputs to try to guess a 6 letters word with 6 tries.
+    
+    Attributes: 
+        user input(str): user type in a capital letter
+         
+    """
+    
+    
+    def __init__():
+        
+        
+    def input():
+        
+    def read_file():
+        
+    def check():
+        
+    def __add__():
+        
+    def game_over():
+        
+    def main()
+        
+        
+        
+    
+        
+        
+    
+    
