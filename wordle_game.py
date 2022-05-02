21e3ab4422eb8cdd4030c039782bb2e00b5fa058
Flugercop <cmadukadaboss@gmail.com>
2022-05-02 10:07:10 -0400

    Moved __name__ == __main__

--- wordle_game.py
+++ wordle_game.py
@@ -13,75 +13,75 @@ import random as rand
 # Create a file of scores, and read in from that file and display it to the player
 
 #Regex Expression for 6 letter words: \b^[a-z]{6}\b
-if __name__ == "__main__":
 
-    class Wordle:
-        """A program that takes in a user inputs to try to guess a 6 letters word
-        6 tries
-        
-        Attributes:
-            name (str): Name of player
-            word (str): Random word
-        """
-            
-        def __init__(self, name, filepath):
-            """ This initilizes a new Wordle object
-            
-            Args:
-                name (str): players name
-                filepath (str): string containing the file path to a wordlist
-                
-            Side effects:
-                Initilizies the name attribute
-                Initilizies the word attribute"""
-            
-        def turn(self):
-            """Simulates a players attempt at guessing the word the Wordle game
-            is thinking of
-            
-            Returns:
-                str: Players guess
-            
-            Raises:
-                ValueError: If user enters a word that does not exist in the list
-            """ 
-        
-        def gameover(self):
-            """ Displays the results to the players
-            
-            Returns:
-                Bool: True if the game is over, false if it is not
-            
-            Side Effects:
-                - Prints out whether or not the player guessed the word correctly
-                - Will write out the number of tries it took a player to guess the 
-                word correctly to a file and store it"""    
-        
-        def match(self, guess, actual_word):
-            """Matches a users guess to the word the game is thinking of
-        
-                
-            Side effects:
-                Prints out whether or not player has guessed a correct letter in the
-                word
-            """
-            
-        
-        def play(self):
-            """
-            
-            Side Effects:
-                Prints out current board
-            """
-            
-            
-    class WorldePractice(Wordle):
-        
-        def __init__(self, name, filepath):
-            
-        
-            
+
+class Wordle:
+    """A program that takes in a user inputs to try to guess a 6 letters word
+    6 tries
     
+    Attributes:
+        name (str): Name of player
+        word (str): Random word
+    """
+        
+    def __init__(self, name, filepath):
+        """ This initilizes a new Wordle object
+        
+        Args:
+            name (str): players name
+            filepath (str): string containing the file path to a wordlist
+            
+        Side effects:
+            Initilizies the name attribute
+            Initilizies the word attribute"""
+        
+    def turn(self):
+        """Simulates a players attempt at guessing the word the Wordle game
+        is thinking of
+        
+        Returns:
+            str: Players guess
+        
+        Raises:
+            ValueError: If user enters a word that does not exist in the list
+        """ 
+    
+    def gameover(self):
+        """ Displays the results to the players
+        
+        Returns:
+            Bool: True if the game is over, false if it is not
+        
+        Side Effects:
+            - Prints out whether or not the player guessed the word correctly
+            - Will write out the number of tries it took a player to guess the 
+            word correctly to a file and store it"""    
+    
+    def match(self, guess, actual_word):
+        """Matches a users guess to the word the game is thinking of
+    
+            
+        Side effects:
+            Prints out whether or not player has guessed a correct letter in the
+            word
+        """
+        
+    
+    def play(self):
+        """
+        
+        Side Effects:
+            Prints out current board
+        """
+        
+        
+class WorldePractice(Wordle):
+    
+    def __init__(self, name, filepath):
+            
+        
+            
+if __name__ == "__main__":   
         
 
         
